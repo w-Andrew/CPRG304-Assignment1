5ae42241673bb89b725210e303a4152412bbdc22
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-19 05:50:20 -0600

    Update SortingUtility.java

--- src/sorting/SortingUtility.java
+++ src/sorting/SortingUtility.java
@@ -2,7 +2,25 @@ package sorting;
 public class SortingUtility {
 
     public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
-
+        int n = arr.length;
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            if (!swapped) {
+                break;
+            }
+        }
     }
 
 
