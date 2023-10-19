5c0ffabf98c2a06241f6e5d05906fd8f14ad8bda
kelvinvu19 <TrungKien.Vu@edu.sait.ca>
2023-10-19 05:48:01 -0600

    sort

---
+++ src/sorting/SortingUtility.java
@@ -0,0 +1,31 @@
+package sorting;
+public class SortingUtility {
+
+    public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
+
+    }
+
+
+    public static <T extends Comparable<T>> void insertionSort(T[] arr) {
+
+    }
+
+    public static <T extends Comparable<T>> void selectionSort(T[] arr) {
+
+    }
+
+
+    public static <T extends Comparable<T>> void mergeSort(T[] arr) {
+
+    }
+
+
+    public static <T extends Comparable<T>> void quickSort(T[] arr) {
+
+    }
+
+
+    public static <T extends Comparable<T>> void customSort(T[] arr) {
+
+    }
+}
