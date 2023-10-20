8240da5a6b6d743397d156d72fcb787fae5c6374
Ivan Templora <ivantemplora@gmail.com>
2023-10-20 11:31:57 -0600

    Update SortingUtility.java
    added insertion sort and selection sort

--- src/sorting/SortingUtility.java
+++ src/sorting/SortingUtility.java
@@ -25,10 +25,34 @@ public class SortingUtility {
 
 
     public static <T extends Comparable<T>> void insertionSort(T[] arr) {
+    	int n = arr.length;
+    	for (int i = 0; i < n; i++) {
+    		T key = arr[i];
+    		int j = i + 1;
+    		
+    		while (j >= 0 && arr[j].compareTo(key) > 0) {
+    			arr[j+1] = arr[j];
+    			j--;
+    		}
+    		arr[j+1] = key;
+    	}
 
     }
 
     public static <T extends Comparable<T>> void selectionSort(T[] arr) {
+    	int n = arr.length;
+    	
+    	for(int i = 0; i < n - 1; i++) {
+    		int min = i;
+    		for(int j = i+1; j < n; j++) {
+    			if(arr[j].compareTo(arr[min]) < 0) {
+    				min = j;
+    			}
+    		}
+    		T temp = arr[i];
+    		arr[i] = arr[min];
+    		arr[min] = temp;
+    	}
 
     }
 
