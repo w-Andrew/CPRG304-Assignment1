2a6d8daa8a46312713db575fc4c3b0717229fae4
Saihaj Mann <saihajmann07@gmail.com>
2023-10-20 19:28:10 -0600

    heapify sort, correction

--- src/sorting/SortingUtility.java
+++ src/sorting/SortingUtility.java
@@ -91,10 +91,10 @@ public class SortingUtility {
     	int largest = i;
     	int left = 2 * i + 1;
     	int right = 2* i + 2;
-    	if(left < lenght && (arr[left].CompareTo(arr[largest]) >0)) {
+    	if(left < lenght && ((arr[left]).compareTo(arr[largest]) >0)) {
     		largest = left;  
     	}
-    	if(left < lenght && (arr[right].CompareTo(arr[largest]) >0)) {
+    	if(left < lenght && (arr[right].compareTo(arr[largest]) >0)) {
        		largest = right;
     	}
     	if(largest != 1) {
